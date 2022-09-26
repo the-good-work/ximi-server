e44898280dba556453a55d21c67edc978480a0a8
siahgoodwork <83395882+siahgoodwork@users.noreply.github.com>
2022-09-26 12:20:57 +0800

    feat: added livekit-server-sdk

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { AccessToken } from "livekit-server-sdk";
+
 const app = express();
 const port = 3000;
 
@@ -6,6 +8,25 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+init();
+
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+function init() {
+  // if this room doesn't exist, it'll be automatically created when the first
+  // client joins
+  const roomName = "name-of-room";
+  // identifier to be used for participant.
+  // it's available as LocalParticipant.identity with livekit-client SDK
+  const participantName = "user-name";
+
+  const at = new AccessToken("api-key", "secret-key", {
+    identity: participantName,
+  });
+  at.addGrant({ roomJoin: true, room: roomName });
+
+  const token = at.toJwt();
+  console.log("access token", token);
+}
