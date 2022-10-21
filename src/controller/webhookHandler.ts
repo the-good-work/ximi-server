93f06d775b0c2ff3614b3aa06eb62c6c34817509
Zahid Yunus <zahid@goodwork.sg>
2022-10-21 09:49:40 +0700

    fix: handler logic

--- src/controller/webhookHandler.ts
+++ src/controller/webhookHandler.ts
@@ -14,19 +14,35 @@ const webhookHandler = async (data: any) => {
         name: data.participant.identity,
         type: data.participant.metadata,
       });
+      if (data.participant.metadata === "CONTROL") {
+        room.controlCount = room.controlCount++;
+      } else if (data.participant.metadata === "OUTPUT") {
+        room.outputCount = room.outputCount++;
+      }
+
+      console.log("room data: ", room);
       await storeRoom(roomName, room);
+      break;
     }
     case "participant_left": {
       room.participants.filter(
         (participant: any) => participant.name !== data.participant.identity
       );
+      if (data.participant.metadata === "CONTROL") {
+        room.controlCount = room.controlCount--;
+      } else if (data.participant.metadata === "OUTPUT") {
+        room.outputCount = room.outputCount--;
+      }
+
+      console.log("room data: ", room);
       await storeRoom(roomName, room);
+      break;
     }
     case "room_finished": {
       await deleteRoom(roomName);
+      break;
     }
   }
-  console.log("room data: ", room);
 };
 
 export { webhookHandler };
